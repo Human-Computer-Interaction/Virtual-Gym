cac9777ce943b4972835ec99b208279212b8ac47
kand7dev <kand7dev@gmail.com>
2024-06-08 03:41:32 +0300

    refactor movement script and add animations

--- Assets/AthleteMovement.cs
+++ Assets/AthleteMovement.cs
@@ -1,22 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class AthleteMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    [SerializeField]
+    private CharacterController controller;
 
-    public float speed = 6f;
-    public float gravity = -9.81f;
+    [SerializeField]
+    private float speed = 1f;
+    [SerializeField]
+    private float gravity = -9.81f;
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private float groundDistance = 0.0f;
+    private LayerMask groundMask;
 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
+    private Vector3 velocity;
+    private bool isGrounded;
 
-    Vector3 velocity;
-    bool isGrounded;
-
+    // Get Animator component
+    private Animator animator;
     // Update is called once per frame
+    void Start()
+    {
+
+        // Get the Animator component
+        animator = GetComponentInChildren<Animator>();
+        groundCheck = GameObject.Find("Ground-Check").GetComponent<Transform>();
+        controller = GetComponent<CharacterController>();
+
+    }
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -25,16 +41,25 @@ public class AthleteMovement : MonoBehaviour
         {
             velocity.y = -2f;
         }
-
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = new Vector3(x, 0, z);
+        if (move == Vector3.zero)
+        {
+            animator.SetFloat("Speed", 0);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0.5f);
+        }
+        move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
     }
 }
