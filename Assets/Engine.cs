5c45350a9f2a082597b64d1494f0eaa9f594c49d
Panos Petropoulos <panospetropoul@gmail.com>
2024-06-10 19:36:06 +0300

    added colliders

---
+++ Assets/Engine.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    // Game Objects Declaration
+
+    [SerializeField] GameObject Athlete;
+    [SerializeField] GameObject Bar;
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager manager;
+
+
+    [SerializeField] GameObject Athlete;
+    [SerializeField] GameObject Bar;
+
+    public void Awake()
+    {
+        if (manager == null)
+        {
+            manager = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (manager != this && manager != null) 
+        {
+            Destroy(this);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collider AthleteCollider = Athlete.GetComponent<Collider>();
+        Collider BarCollider = Bar.GetComponent<Collider>();
+
+        if (AthleteCollider != null && BarCollider != null)
+        {
+            if (AthleteCollider.bounds.Intersects(BarCollider.bounds))
+            {
+                Debug.Log("Collision detected!");
+            }
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
