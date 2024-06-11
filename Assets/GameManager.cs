3faf678bd69c2074eed469ab835581b977d665b6
Panos Petropoulos <panospetropoul@gmail.com>
2024-06-11 16:54:44 +0300

    BMI add

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,7 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Linq;
 using UnityEngine;
+public struct PlayerStats
+{
+   
+    public float Weight;
+    public float Stamina;
+    public float Money;
+    public float Height;
+    public int Age;
 
+    
+
+    public float CaloriesBurned ()
+    {
+        return 0.0f;
+    }
+    
+
+
+
+    public PlayerStats(float weight, float stamina, float money, float height, int age)
+    {
+        this.Weight = weight;
+        this.Stamina = stamina;
+        this.Money = money;
+        this.Height = height;
+        this.Age = age;
+
+
+
+
+    }
+
+}
 public class GameManager : MonoBehaviour
 {
     public static GameManager manager;
@@ -13,7 +45,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject Bar;
     [SerializeField] GameObject LegExtension;
     [SerializeField] GameObject Bike;
-    [SerializeField] GameObject Bike1;
+    //[SerializeField] GameObject Bike1;
     [SerializeField] GameObject Treadmill1;
     [SerializeField] GameObject Treadmill2;
     [SerializeField] GameObject Mat;
@@ -24,12 +56,23 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject WeightTrack;
     [SerializeField] GameObject Ball12;
 
+  
+    private Collider BarCollider;
+    private Collider AthleteCollider;
+
+    public PlayerStats playerStats;
+    public float Timer = 0f;
+
+    public float BMI;
+    
+
     public void Awake()
     {
         if (manager == null)
         {
             manager = this;
             DontDestroyOnLoad(this);
+
         }
         else if (manager != this && manager != null) 
         {
@@ -38,11 +81,11 @@ public class GameManager : MonoBehaviour
     }
     private void CheckCollision()
     {
-        Collider AthleteCollider = Athlete.GetComponent<Collider>();
-        Collider BarCollider = Bar.GetComponent<Collider>();
+        AthleteCollider = Athlete.GetComponent<Collider>();
+        BarCollider = Bar.GetComponent<Collider>();
         Collider LegCollider = LegExtension.GetComponent<Collider>();
         Collider BikeCollider = Bike.GetComponent<Collider>();
-        Collider Bike1Collider = Bike1.GetComponent<Collider>();
+        //Collider Bike1Collider = Bike1.GetComponent<Collider>();
         Collider Treadmill1Collider = Treadmill1.GetComponent<Collider>();
         Collider Treadmill2Collider = Treadmill2.GetComponent<Collider>();
         Collider WeightScaleCollider = WeightScale.GetComponent<Collider>();
@@ -55,16 +98,23 @@ public class GameManager : MonoBehaviour
 
         if (AthleteCollider != null && BarCollider != null)
             if (AthleteCollider.bounds.Intersects(BarCollider.bounds))
-                Debug.Log("Collision detected in Bar!!");
+                {
+                    Timer += Time.deltaTime;
+                    print("Timer: " + Timer);
+                }
+                
         if (AthleteCollider != null && LegCollider != null)
             if (AthleteCollider.bounds.Intersects(LegCollider.bounds))
-                Debug.Log("Collision detected in Leg Extension!!");
+                {
+                    Timer += Time.deltaTime;
+                    print("Timer: " + Timer);
+                }
         if (AthleteCollider != null && BikeCollider != null)
             if (AthleteCollider.bounds.Intersects(BikeCollider.bounds))
                 Debug.Log("Collision detected in Bike!!");
-        if (AthleteCollider != null && Bike1Collider != null)
-            if (AthleteCollider.bounds.Intersects(Bike1Collider.bounds))
-                Debug.Log("Collision detected in Bike1!!");
+        // if (AthleteCollider != null && Bike1Collider != null)
+        //     if (AthleteCollider.bounds.Intersects(Bike1Collider.bounds))
+        //         Debug.Log("Collision detected in Bike1!!");
         if (AthleteCollider != null && Treadmill1Collider != null)
             if (AthleteCollider.bounds.Intersects(Treadmill1Collider.bounds))
                 Debug.Log("Collision detected in Treadmill1!!");
@@ -94,13 +144,64 @@ public class GameManager : MonoBehaviour
                 Debug.Log("Collision detected in Ball12!!");
     }
 
+    private float CalculateBMI(PlayerStats playerStats)
+    {
+        return playerStats.Weight / (playerStats.Height * playerStats.Height); 
+    }
+
+    private String BodyTypeBasedOnBmi()
+    {
+        if (BMI < 18.5)
+        {
+            return "Underweight";
+        }
+        else if (BMI >= 18.5 && BMI <= 24.9)
+        {
+            return "Normal";
+        }
+        else if (BMI >= 25 && BMI <= 29.9)
+        {
+            return "Overweight";
+        }
+        else if (BMI >= 30)
+        {
+            return "Obese";
+        }
+
+        return "";
+    }
+
+
     void Start()
     {
+        
+        playerStats = new PlayerStats(70f, 100f, 100f, 1.80f, 30);
+        BMI = CalculateBMI(playerStats);
+        print("BMI "+BMI);
+        String bodyType =  BodyTypeBasedOnBmi();
+        print("BodtType: " + bodyType);
+        
+       // CalculateBMI();
     }
 
 
     void Update()
     {
+        
+
         CheckCollision();
+        //CheckTrigger();
+        
+        print("Timer: " + Timer);
     }
+
+    // private void CheckTrigger()
+    // {
+        
+        
+    //     if (AthleteCollider.bounds.Intersects(BarCollider.bounds))
+    //     {
+    //         Timer += Time.deltaTime;
+    //     }
+    // }
 }
