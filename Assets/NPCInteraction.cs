9bbf1e5d58149fba1be6217ba04b48646f7f54e6
kand7dev <kand7dev@gmail.com>
2024-06-11 18:37:27 +0300

    implement half interaction with npc

--- Assets/NPCInteraction.cs
+++ Assets/NPCInteraction.cs
@@ -1,34 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class NPCInteraction : MonoBehaviour
 {
-    private GameObject dialoguePanel;
-    private Text dialogueText;
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+    public GameObject continueButton;
 
     public string[] dialogue;
     private int index;
     public float wordSpeed;
-    public bool playerIsClose = false;
-    void Start()
-    {
-        dialoguePanel = GameObject.Find("DialoguePanel");
-        dialoguePanel.SetActive(false);
-
-    }
+    public bool playerIsClose;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Athlete"))
         {
-            dialoguePanel.SetActive(true);
+            playerIsClose = true;
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Athlete"))
         {
-            dialoguePanel.SetActive(false);
+            playerIsClose = false;
+            ZeroText();
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                ZeroText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+        if (dialogueText.text == dialogue[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+    public void ZeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+    public void NextLine()
+    {
+        continueButton.SetActive(false);
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            ZeroText();
+        }
+
+    }
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
         }
     }
 }
