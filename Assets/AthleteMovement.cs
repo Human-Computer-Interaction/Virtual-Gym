5a0387664dec524c802e4baecb13cfd06f6f2438
Panos Petropoulos <panospetropoul@gmail.com>
2024-06-12 18:01:39 +0300

    tried to add running animation on treadmills

--- Assets/AthleteMovement.cs
+++ Assets/AthleteMovement.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class AthleteMovement : MonoBehaviour
+
+
 {
     [SerializeField]
     private CharacterController controller;
@@ -23,7 +27,37 @@ public class AthleteMovement : MonoBehaviour
 
     // Get Animator component
     private Animator animator;
+
+    private bool isOnTTreadmille = false;
     // Update is called once per frame
+
+    [SerializeField] GameObject Treadmill1;
+    [SerializeField] GameObject Treadmill2;
+    
+       void CheckCollision()
+    {
+        Collider Treadmill1Collider = Treadmill1.GetComponent<Collider>();
+        Collider Treadmill2Collider = Treadmill2.GetComponent<Collider>();
+
+        Collider AthleteCollider = GetComponent<Collider>();
+        if (Treadmill1Collider != null && Treadmill2Collider != null)
+        {
+            if (AthleteCollider.bounds.Intersects(Treadmill2Collider.bounds) ||
+             AthleteCollider.bounds.Intersects(Treadmill1Collider.bounds))
+            {
+                isOnTTreadmille = true;
+                animator.SetBool("isRunning", true);
+                speed = 0.000000000001f;
+            }
+            else
+            {
+                isOnTTreadmille = false;
+                animator.SetBool("isRunning", false);
+                speed = 1f;
+                
+            }
+        }
+    }
     void Start()
     {
 
@@ -33,8 +67,11 @@ public class AthleteMovement : MonoBehaviour
         controller = GetComponent<CharacterController>();
 
     }
+    
     void Update()
     {
+        CheckCollision();
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
@@ -53,6 +90,7 @@ public class AthleteMovement : MonoBehaviour
         {
             animator.SetFloat("Speed", 0.5f);
         }
+        
         move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
@@ -62,4 +100,5 @@ public class AthleteMovement : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
 
     }
+  
 }
