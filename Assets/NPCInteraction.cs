d6f097ed8ef12db11ee3c303b855f196dc85ae4e
kand7dev <kand7dev@gmail.com>
2024-06-13 13:33:23 +0300

    set camera angle manually and disable camera follow when talking with npc

--- Assets/NPCInteraction.cs
+++ Assets/NPCInteraction.cs
@@ -16,6 +16,8 @@ public class NPCInteraction : MonoBehaviour
     public GameObject characteristicsInputPanel;
     public GameObject gameManagerComponent;
     private GameManager gameManager;
+    private GameObject playerCameraRotation;
+    private GameObject playerCamera;
 
     private List<string> dialogue;
     private int index;
@@ -39,7 +41,10 @@ public class NPCInteraction : MonoBehaviour
     }
     void Start()
     {
+        playerCameraRotation = GameObject.Find("Athlete");
+        playerCamera = GameObject.Find("Camera");
         gameManager = gameManagerComponent.GetComponent<GameManager>();
+
         dialogue = new List<string>
         {
             "Καλώς ήρθατε στο Γυμναστήριο!",
@@ -50,6 +55,9 @@ public class NPCInteraction : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
         {
+            playerCameraRotation.GetComponentInChildren<MouseLook>().enabled = false;
+            // 88f is the y rotation of the camera, not 0 because we're rotating the camera when the game starts.
+            playerCamera.transform.rotation = Quaternion.Euler(new Vector3(12f, 88f, 0f));
             if (dialoguePanel.activeInHierarchy)
             {
                 ZeroText();
@@ -64,13 +72,14 @@ public class NPCInteraction : MonoBehaviour
         {
             continueButton.SetActive(true);
         }
-        //if index == 
+       
     }
     public void ZeroText()
     {
         dialogueText.text = "";
         index = 0;
         dialoguePanel.SetActive(false);
+        playerCameraRotation.GetComponentInChildren<MouseLook>().enabled = true;
     }
     public void NextLine()
     {
@@ -84,7 +93,7 @@ public class NPCInteraction : MonoBehaviour
         else if (index == dialogue.Count - 1)
         {
             dialoguePanel.SetActive(false);
-            if(!hasCharacteristics)
+            if (!hasCharacteristics)
                 characteristicsInputPanel.SetActive(true);
             //PlayerStats playerStats = new PlayerStats(70f, 100f, 100f, 1.80f, 30);
         }
@@ -94,11 +103,11 @@ public class NPCInteraction : MonoBehaviour
         var inputsFields = characteristicsInputPanel.GetComponentsInChildren<TMP_InputField>();
         gameManager.playerStats.Weight = Math.Abs(float.Parse(inputsFields[0].text));
         gameManager.playerStats.Age = Math.Abs(int.Parse(inputsFields[1].text));
-        gameManager.playerStats.Height = Math.Abs(float.Parse(inputsFields[2].text))/100f;
+        gameManager.playerStats.Height = Math.Abs(float.Parse(inputsFields[2].text)) / 100f;
         gameManager.InitStats();
         gameManager.ActivatePanel();
-        
-        
+
+
         characteristicsInputPanel.SetActive(false);
         inputsFields[0].text = "";
         inputsFields[1].text = "";
@@ -114,16 +123,16 @@ public class NPCInteraction : MonoBehaviour
     public void GymPlanBasedOnBMI()
     {
         dialogue = new List<string>();
-        print("size of dialogue = "+dialogue.Count());
+        print("size of dialogue = " + dialogue.Count());
         gameManager.CalculateBMI(gameManager.playerStats);
         String BodyType = gameManager.BodyTypeBasedOnBmi();
         print(BodyType);
-        switch(BodyType)
+        switch (BodyType)
         {
-           
+
             case "Underweight":
                 {
-                    
+
                     dialogue.Add("You are Underweight so I created a gym program for you.");
                     dialogue.Add("This program will help you gain muscle , but you will have to eat More");
                     dialogue.Add("Ask The nutritionist for more info about the food");
@@ -138,8 +147,8 @@ public class NPCInteraction : MonoBehaviour
                     break;
                 }
             case "Normal":
-            {
-                    
+                {
+
                     dialogue.Add("You have Normal weight.");
                     dialogue.Add("This program will help you gain muscle , but you will have to eat More");
                     dialogue.Add("Ask The nutritionist for more info about the food");
@@ -152,10 +161,10 @@ public class NPCInteraction : MonoBehaviour
                     gameManager.equipmentUse.SquatUse = 10f;
                     gameManager.equipmentUse.LegExtensionUse = 10f;
                     break;
-            }
+                }
             case "Overweight":
-            {
-                    
+                {
+
                     dialogue.Add("You are OverWeight so I created a gym program for you.");
                     dialogue.Add("This program will help you to lose weight , but you will have to eat Less");
                     dialogue.Add("Ask The nutritionist for more info about the food");
@@ -168,10 +177,10 @@ public class NPCInteraction : MonoBehaviour
                     gameManager.equipmentUse.SquatUse = 10f;
                     gameManager.equipmentUse.LegExtensionUse = 10f;
                     break;
-            }
+                }
             case "Obese":
-            {
-                    
+                {
+
                     dialogue.Add("You are Obese so I created a gym program for you.");
                     dialogue.Add("This program will help you lose weight , but you will have to eat less");
                     dialogue.Add("Ask The nutritionist for more info about the food");
@@ -185,8 +194,8 @@ public class NPCInteraction : MonoBehaviour
                     gameManager.equipmentUse.LegExtensionUse = 10f;
                     gameManager.equipmentUse.DumbellsUse = 10f;
                     break;
-            }
-            
+                }
+
 
         }
         print(dialogue.Count());
