4d252cc559cfe6972c7db9f707819539a51ff824
geotheo01 <theogeorge80@gmail.com>
2024-06-28 00:16:48 +0300

    code

--- Assets/PurchaseGymProducts.cs
+++ Assets/PurchaseGymProducts.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PurchaseGymProducts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class GymProduct
     {
-        
+        public string productName;
+        public float price;
+        public string buff;
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<GymProduct> gymProducts = new List<GymProduct>();
+    public GameObject productButtonPrefab;
+    public Transform productsPanel;
+    public Text selectedProductText;
+    public Text totalPriceText;
+
+    private float totalPrice = 0f;
+    private GymProduct selectedProduct;
+
+    void Start()
     {
-        
+        // Δημιουργία παραδειγμάτων προϊόντων
+        gymProducts.Add(new GymProduct { productName = "Energy Drink", price = 5f, buff = "+10% Energy" });
+        gymProducts.Add(new GymProduct { productName = "Protein", price = 20f, buff = "+15% Strength" });
+        gymProducts.Add(new GymProduct { productName = "Creatinine", price = 30f, buff = "+10% Stamina" });
+        gymProducts.Add(new GymProduct { productName = "Gym Gloves", price = 10f, buff = "+5% Grip" });
+        gymProducts.Add(new GymProduct { productName = "Dipping Belt", price = 25f, buff = "+20% Stability" });
+
+        // Εμφάνιση των προϊόντων
+        DisplayProducts();
+    }
+
+    void DisplayProducts()
+    {
+        foreach (GymProduct product in gymProducts)
+        {
+            GameObject button = Instantiate(productButtonPrefab, productsPanel);
+            button.GetComponentInChildren<Text>().text = $"{product.productName} - ${product.price} ({product.buff})";
+            button.GetComponent<Button>().onClick.AddListener(() => SelectProduct(product));
+        }
+    }
+
+    void SelectProduct(GymProduct product)
+    {
+        selectedProduct = product;
+        selectedProductText.text = $"Selected: {product.productName} - ${product.price} ({product.buff})";
+    }
+
+    public void PurchaseProduct()
+    {
+        if (selectedProduct != null)
+        {
+            totalPrice += selectedProduct.price;
+            totalPriceText.text = $"Total: ${totalPrice}";
+            selectedProductText.text = "Selected: None";
+            selectedProduct = null;
+        }
     }
 }
