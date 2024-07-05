fe80cc205298841ab36693b801cbe29d92fe19c4
geotheo01 <theogeorge80@gmail.com>
2024-07-05 16:01:39 +0300

    οκ

--- Assets/NutrionistInteraction.cs
+++ Assets/NutrionistInteraction.cs
@@ -26,6 +26,7 @@ public class NutrionistInteraction : MonoBehaviour
             playerIsClose = true;
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Athlete"))
@@ -41,12 +42,53 @@ public class NutrionistInteraction : MonoBehaviour
         playerCamera = GameObject.Find("Camera");
         gameManager = gameManagerComponent.GetComponent<GameManager>();
 
+        if (gameManager == null)
+        {
+            Debug.LogError("GameManager component is not assigned!");
+            return;
+        }
+
+        InitializeDialogue();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        {
+            HandleInteraction();
+        }
+
+        if (dialogueText.text == dialogue[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    private void HandleInteraction()
+    {
+        playerCameraRotation.GetComponentInChildren<MouseLook>().enabled = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        playerCamera.transform.rotation = Quaternion.Euler(new Vector3(12f, 88f, 0f));
+
+        if (dialoguePanel.activeInHierarchy)
+        {
+            ZeroText();
+        }
+        else
+        {
+            dialoguePanel.SetActive(true);
+            StartCoroutine(Typing());
+        }
+    }
+
+    private void InitializeDialogue()
+    {
         if (gameManager.physicalCondition == "Unknown")
         {
             dialogue = new List<string>
             {
                 "Welcome to PADA Gym!",
-                "You should talk to the fitness instructor to check your physical condition.",
+                "You should talk to the fitness instructor to check your physical condition."
             };
         }
         else
@@ -56,30 +98,6 @@ public class NutrionistInteraction : MonoBehaviour
                 "Welcome to PADA Gym!",
                 "You have already checked your physical condition."
             };
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
-        {
-            playerCameraRotation.GetComponentInChildren<MouseLook>().enabled = false;
-            Cursor.lockState = CursorLockMode.Confined;
-            playerCamera.transform.rotation = Quaternion.Euler(new Vector3(12f, 88f, 0f));
-            if (dialoguePanel.activeInHierarchy)
-            {
-                ZeroText();
-            }
-            else
-            {
-                dialoguePanel.SetActive(true);
-                StartCoroutine(Typing());
-            }
-        }
-        if (dialogueText.text == dialogue[index])
-        {
-            continueButton.SetActive(true);
         }
     }
 
@@ -104,11 +122,14 @@ public class NutrionistInteraction : MonoBehaviour
         else if (index == dialogue.Count - 1)
         {
             dialoguePanel.SetActive(false);
+            // Στο τέλος του διαλόγου καλείται η μέθοδος PassInputsToGameManager
+            PassInputsToGameManager();
         }
     }
 
     public void PassInputsToGameManager()
     {
+        Debug.Log("Calling PassInputsToGameManager");
         gameManager.ActivatePanel();
         FoodProgramBasedOnPhysicalCondition();
     }
@@ -116,58 +137,54 @@ public class NutrionistInteraction : MonoBehaviour
     public void FoodProgramBasedOnPhysicalCondition()
     {
         dialogue.Clear();
-        print(gameManager.physicalCondition);
-        switch(gameManager.physicalCondition)
+        Debug.Log("Physical Condition: " + gameManager.physicalCondition); // Για έλεγχο της τιμής
+
+        switch (gameManager.physicalCondition)
         {
             case "Underweight":
-            {
                 dialogue.Add("You are 'Underweight' so I created a food program for you.");
                 dialogue.Add("The fitness instructor gave you a program that will help you gain muscle, but you will have to eat more.");
                 dialogue.Add("Let me show you what to buy from the vending machine.");
                 dialogue.Add("Your food program is ready.");
                 dialogue.Add("5 Protein, 5 Creatine, 4 Energy Drinks, 1 pair of Gloves, 1 Dipping Zone");
                 dialogue.Add("Πάρε κανά κιλό ρε χτικιάρη!!");
-
                 break;
-            }
+
             case "Normal":
-            {
                 dialogue.Add("You are 'Normal' so I created a food program for you.");
                 dialogue.Add("The fitness instructor gave you a program that will help you maintain your weight.");
                 dialogue.Add("Let me show you what to buy from the vending machine.");
                 dialogue.Add("Your food program is ready.");
                 dialogue.Add("5 Protein, 1 Energy Drink, 1 Dipping Zone");
                 dialogue.Add("Είσαι Αστέρι!!");
-
                 break;
-            }
+
             case "Overweight":
-            {
                 dialogue.Add("You are 'Overweight' so I created a food program for you.");
                 dialogue.Add("The fitness instructor gave you a program that will help you to lose weight, but you will have to eat less.");
                 dialogue.Add("Let me show you what to buy from the vending machine.");
                 dialogue.Add("Your food program is ready.");
                 dialogue.Add("1 Protein, 1 Creatine, 5 Energy Drink, 1 Gloves, 1 Dipping Zone");
                 dialogue.Add("Χάσε κανά κιλό ρε χοντρέ!!");
-
                 break;
-            }
+
             case "Obese":
-            {
                 dialogue.Add("You are 'Obese' so I created a food program for you.");
                 dialogue.Add("The fitness instructor gave you a program that will help you to lose weight, but you will have to eat less.");
                 dialogue.Add("Let me show you what to buy from the vending machine.");
                 dialogue.Add("Your food program is ready.");
                 dialogue.Add("10 Energy Drink, 1 Gloves, 1 Dipping Zone");
                 dialogue.Add("Ε ρε τι κάνουν τα McDonalds!!");
-
                 break;
-            }
 
-
+            default:
+                dialogue.Add("You need to check your physical condition with the fitness instructor first.");
+                break;
         }
+
         dialogueText.text = dialogue[0];
         dialoguePanel.SetActive(true);
+        StartCoroutine(Typing());
     }
 
     IEnumerator Typing()
